52b034b182f96dd5b3e7cc1acfbc193ef60fbbcd
Ali Sal <ali@moveaheadmedia.co.uk>
2021-12-08 09:54:28 +0700

    Added fix-root-page-copies.php

---
+++ fix-root-page-copies.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Move Ahead Media Fix Root Page Copies
+ * Plugin URI: https://github.com/moveaheadmedia/fix-root-page-copies/
+ * Description: WordPress plugin to fix root page copies (redirect root pages to the correct permalink)
+ * Version: 1.0
+ * Author: Move Ahead Media
+ * Author URI: https://github.com/moveaheadmedia/
+ *
+ */
+

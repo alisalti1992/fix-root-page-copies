922a0286b914d674fa7d3239841e476a59ff1516
Ali Sal <ali@moveaheadmedia.co.uk>
2021-12-08 10:30:56 +0700

    Version 1.0 Ready

--- fix-root-page-copies.php
+++ fix-root-page-copies.php
@@ -8,3 +8,18 @@
  * Author URI: https://github.com/moveaheadmedia/
  *
  */
+
+
+add_action('template_redirect', 'mam_fix_root_page_copies');
+
+function mam_fix_root_page_copies()
+{
+    if(is_single() || is_page()){
+        $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        if (strpos($actual_link, get_permalink()) == false && get_permalink() != $actual_link) {
+            wp_redirect( get_permalink() );
+            exit;
+        }
+    }
+}
+
